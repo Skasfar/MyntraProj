0c48ae984d11da04407ce7a8f528207ae61dbbec
Skasfar <sk.md.asfar@gmail.com>
2023-03-09 01:45:32 +0530

    optimized

---
+++ src/Home.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import Connection from "./main/Connection";
+import Slider from "./main/Slider";
+import Myntra from "./main/Myntra"
+import { useEffect, useState } from "react";
+import ProductArray from "./main/ProductArray";
+import axios from 'axios';
+import Banner from './main/Banner';
+const Home = () => {
+    let [products, setProducts] = useState([]);
+    let [products1, setProducts1] = useState([]);
+    useEffect(() => {
+        //****mens clothing****
+        const options = {
+            method: 'GET',
+            url: 'https://fakestoreapi.com/products/category/men%27s%20clothing'
+        };
+        axios.request(options).then(function (response) {
+            setProducts(response.data)
+            // console.log(response.data);
+        }).catch(function (error) {
+            console.error(error);
+        });
+        //******womens clothing****
+        const options1 = {
+            method: 'GET',
+            url: `https://fakestoreapi.com/products/category/women%27s%20clothing`
+        }
+        axios.request(options1).then(function (response) {
+            setProducts1(response.data)
+            // console.log(response.data);
+        }).catch(function (error) {
+            console.error(error);
+        });
+
+    }, [])
+
+
+    return (
+        <div>
+            <Myntra />
+            <Slider />
+            <Banner data="https://assets.myntassets.com/w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2023/3/2/9063526d-1fcc-4002-a890-36dae5b4473f1677742079670-03_OMG--Deals.jpg" />
+            
+            <ProductArray data={products1}/>
+            <Banner data="https://assets.myntassets.com/w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2023/3/5/5725220e-ef3c-40c3-a60d-1e5361a3aa5e1678037704201-Desktop-Banner_Unisex1.png"/>
+            {/* <Connection/> */}
+
+        </div>
+    )
+}
+
+export default Home
---
+++ src/main/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Banner = (s) => {
+
+  return (
+    <div>
+        <img src={s.data} alt="" />
+    </div>
+  )
+}
+
+export default Banner
