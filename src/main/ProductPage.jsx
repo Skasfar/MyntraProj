05191383c4570c3e25251ec6a72c8a15cddf57ec
Skasfar <sk.md.asfar@gmail.com>
2023-03-09 23:50:14 +0530

    Brawser Router instaled

---
+++ src/main/ProductPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Myntra from './Myntra'
+
+const ProductPage = () => {
+  return (<div>
+   
+    <div>ProductPage </div>
+    </div>
+  )
+}
+
+export default ProductPage
